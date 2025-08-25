f3f931e634725b1793b2c1dfa545bf3d1cc46086
kjminn <kjminn2001@gmail.com>
2025-08-25 16:20:19 +0900

    DDL - alter table2, DML - insert table 고객 , 전체고객조회

--- 0825.sql
+++ 0825.sql
@@ -48,4 +48,34 @@ alter table 고객
 alter table 고객 
                 drop column 가입날짜;
                 
---테이블 변경: 제약조건 추가                
+--테이블 변경: 체크 제약조건 추가                
+
+alter table 고객
+                add CONSTRAINT check_age check(나이>=20);
+                
+                
+--테이블 변경: 체크 제약조건 삭제
+
+alter table 고객
+                drop constraint check_age;
+                
+--테이블 삭제
+
+drop table 배송업체;
+
+-- DML(데이터 조작어)
+
+-- 고객 테이블에 행 삽입(추가)
+
+insert into 고객 values('apple', '정소화', 20, 'gold', '학생', 1000);
+insert into 고객 values('banana', '김선우', 25, 'vip', '간호사', 2500);
+insert into 고객 values('carrot', '고명석', 28, 'gold', '교사', 4500);
+insert into 고객 values('orange', '김용욱', 22, 'silver', '학생', 0);
+insert into 고객 values('melon', '성원용', 35, 'gold', '회사원', 5000);
+insert into 고객(고객아이디, 고객이름, 등급, 직업, 적립금) values('peach', '오형준', 'gold', '교사', 4500);
+insert into 고객 values('pear', '채광주', 31, 'silver', '회사원', 500);
+
+
+-- 고객테이블의 전체 행 조회(검색)
+
+select * from 고객;
